9d1467e16c4c30210c9da8e9155a57cf7eaadf29
Dhirachat Chayaporn <omiejung@gmail.com>
2026-06-14 22:26:26 +0700

    My R Programming Code for Quantitative Research - 04 - finish clip: Creating a summary statistics table

--- Online_Course/FWB_data_analysis/fwd_code.R
+++ Online_Course/FWB_data_analysis/fwd_code.R
@@ -4,7 +4,6 @@ install.packages("dplyer")
 library(dplyr)
 
 #Importing FWB dataset
-
 fwbdata <- read.csv("https://www.consumerfinance.gov/documents/5614/NFWBS_PUF_2016_data.csv")
 
 #subset: less than $50 income
@@ -12,10 +11,31 @@ lowinc <- fwbdata %>% filter(PPINCIMP<=4)
 
 table(lowinc$PPINCIMP)
 
-lowinc2 <- lowinc %>% select(PPGENDER,PPHHSIZE,PPINCIMP)
+lowinc2 <- lowinc %>% select(PPGENDER,PPHHSIZE,PPINCIMP, FWBscore, finalwt)
 
 #creating a smaller_house dummy variable
-
 lowinc2$smaller_house <- ifelse(lowinc2$PPHHSIZE <= 2, 1,0)
+
+#Renaming the levels of the PPGENDER variable
+lowinc2$PPGENDER <- recode(lowinc2$PPGENDER, "1"="Male", "2"="Female")
+
+#Creating a new categorical variable at the intersection of PPGENDER and small_house
+lowinc2$gen_small_house <- ifelse(lowinc2$PPGENDER=="Female" & lowinc2$smaller_house==1, "Female living in a smaller house",
+                                  ifelse(lowinc2$PPGENDER=="Female" & lowinc2$smaller_house==0, "Female living in a larger house",
+                                         ifelse(lowinc2$PPGENDER=="Male" & lowinc2$smaller_house==1, "Male living in a smaller house",
+                                                "Male living in a larger house")
+                                  )
+                            )
+
+
+#Creating a summary statistics table
+#sum_stat_tab <- lowinc2 %>% group_by(gen_small_house) %>% 
+sum_stat_tab <- lowinc2 %>% group_by(PPGENDER, smaller_house) %>% 
+                            summarise(Average_FWBscore= round(mean(FWBscore), digits=1),
+                                      Average_FWBscore_weighted= round(weighted.mean(FWBscore, finalwt), digits=1),
+                                      Median_FWBscore = round(median(FWBscore), digits=1),
+                                      SD_FWBscore= round(sd(FWBscore), digits=1))
+
+
 
 
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # R_Survey_Code
 This is a R programming to do the job like SPSS/SAS.
-My R Programming Code for Quantitative Research - 03
+My R Programming Code for Quantitative Research - 04 - finish clip: Creating a summary statistics table
 
 # todo
 Craete Code Book
