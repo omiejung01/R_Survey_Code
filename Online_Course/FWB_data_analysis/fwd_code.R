dd69cee39e2b5b5b869a6e0394699662e585ab10
Dhirachat Chayaporn <omiejung@gmail.com>
2026-06-15 13:49:21 +0700

    My R Programming Code for Quantitative Research - 05 - finish clip: Creating a grouped/stacked barplot

--- Online_Course/FWB_data_analysis/fwd_code.R
+++ Online_Course/FWB_data_analysis/fwd_code.R
@@ -2,6 +2,7 @@
 install.packages("dplyer")
 
 library(dplyr)
+library(ggplot2)
 
 #Importing FWB dataset
 fwbdata <- read.csv("https://www.consumerfinance.gov/documents/5614/NFWBS_PUF_2016_data.csv")
@@ -27,15 +28,30 @@ lowinc2$gen_small_house <- ifelse(lowinc2$PPGENDER=="Female" & lowinc2$smaller_h
                                   )
                             )
 
+#Renaming the levels of the PPINCIMP variable
+lowinc2$PPINCIMP <- recode(lowinc2$PPINCIMP, "1"="Less than $20,000",
+                           "2"="$20,000 to $29,999",
+                           "3"="$30,000 to $39,999",
+                           "4"="$40,000 to $49,999")
+
 
 #Creating a summary statistics table
-#sum_stat_tab <- lowinc2 %>% group_by(gen_small_house) %>% 
-sum_stat_tab <- lowinc2 %>% group_by(PPGENDER, smaller_house) %>% 
+sum_stat_tab <- lowinc2 %>% group_by(gen_small_house, PPINCIMP) %>% 
+#sum_stat_tab <- lowinc2 %>% group_by(PPGENDER, smaller_house) %>% 
                             summarise(Average_FWBscore= round(mean(FWBscore), digits=1),
                                       Average_FWBscore_weighted= round(weighted.mean(FWBscore, finalwt), digits=1),
                                       Median_FWBscore = round(median(FWBscore), digits=1),
                                       SD_FWBscore= round(sd(FWBscore), digits=1))
 
 
+ggplot(sum_stat_tab, aes(x=gen_small_house, y=Average_FWBscore_weighted, fill=PPINCIMP)) +
+  geom_bar(stat="identity", position = "dodge")+
+  coord_flip()+
+  labs(x=" ", y="Average Financial wellbeing score", fill="Household Income", fill="Household income")+
+  theme_minimal()
+
+
+
+  
 
 
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # R_Survey_Code
 This is a R programming to do the job like SPSS/SAS.
-My R Programming Code for Quantitative Research - 04 - finish clip: Creating a summary statistics table
+My R Programming Code for Quantitative Research - 05 - finish clip: Creating a grouped/stacked barplot
 
 # todo
 Craete Code Book
